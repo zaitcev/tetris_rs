8acf296e3975dcde6db34404226b186bfc5b205e
Pete Zaitcev <zaitcev@kotori.zaitcev.us>
2026-04-25 22:48:06 -0500

    Give up, use termion

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,62 @@
-use std::io;
-use std::io::Read;  // the trait Read provides read_exact
+// use std::io;
+// use std::io::Read;  // the trait Read provides read_exact
+use std::io::{stdin, stdout, Write};
 use std::process::ExitCode;
+use termion::event::Key;
+use termion::input::TermRead; // provides keys()
+use termion::raw::IntoRawMode;
+
+// struct
+// ....
+//    array Matrix
+
 
 fn main() -> ExitCode {
-    println!("Hello, world!");
 
-    let mut inp = io::stdin();
-    let mut buffer : [u8; 1] = [0];
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    let stdin = stdin();
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    stdout.flush().unwrap();
 
-    if let Err(e) = inp.read_exact(&mut buffer) {
-        eprintln!("read_exact error: {}", e);
-        return ExitCode::from(1);
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('q') => {
+                break;
+            }
+            Key::Char('q') => {
+                break;
+            }
+            Key::Left => {
+                stdout.write(b"<").unwrap();
+            }
+            Key::Right => {
+                stdout.write(b">").unwrap();
+            }
+            _ => {}
+        }
+        stdout.flush().unwrap();
     }
-    print!("read {}\n", buffer.len());
 
+    // XXX Restore terminal to cooked before printing this.
+    println!("Goodbye");
     ExitCode::from(0)
 }
+
+//
+//    let size = termion::terminal_size().unwrap();
+//    Self {
+//        terminal_size: Coordinates {
+//            x: size.0 as usize,
+//            y: size.1 as usize,
+//        },
+//    }
+
+//    fn set_pos(&mut self, x: usize, y: usize) {
+//        self.cur_pos.x = x;
+//        self.cur_pos.y = y;
+//        println!(
+//            "{}",
+//            termion::cursor::Goto(
+//                self.cur_pos.x as u16, (self.cur_pos.y) as u16)
+//        );
+//    }
