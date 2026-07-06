9104853abb23bc81703270df88d7d1d88804536e
Pete Zaitcev <zaitcev@kotori.zaitcev.us>
2026-07-05 21:44:42 -0500

    WIP first figure and update

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,77 @@ impl<'a> Display for DisplayOne<'a> {
         let s = format!("{}{}", termion::cursor::Goto(1, 1), msg);
         self.dp.write(s.as_bytes()).unwrap();
     }
+    fn update(&mut self, &can: Can, &curfig: Figure) {
+        // XXX Later
+        let points = [Point(0,0), 4];
+        // XXX if OK(curfig)
+        curfig.land(points);
+
+        let field = vec![vec![false; COLS]; ROWS];
+        // XXX smash with can
+
+        for i in 0..4 {
+            let p = points[i];
+            field[p.row][p.col] = true;
+        }
+
+        for i in 0..ROWS {
+            let s = format!("{}", termion::cursor::Goto(
+                    (TCOFF+1) as u16, (TROFF+i+1) as u16));
+            self.dp.write(s.as_bytes()).unwrap();
+            // XXX optimize
+            for j in 0..COLS {
+                if field[i][j] {
+                    let s = "[_]";
+                    self.dp.write(s.as_bytes()).unwrap();
+                } else {
+                    let s = " . ";
+                    self.dp.write(s.as_bytes()).unwrap();
+                }
+            }
+        }
+    }
+}
+
+struct Point {
+    col: u16,
+    row: u16,
+}
+
+pub trait Figure {
+    fn init(&self, cols: u16, rows: u16);
+    fn land(&self, rv: &mut Point[4]);
+}
+
+struct barFigure {
+    pos: Point,
+    points: [Point, 3],
+}
+
+enum GenericFigure {
+   bar(BarFigure),
+}
+
+impl Figure for GenericFigure {
+    fn init(&self, cols: u16, rows: u16) {
+        self.pos = Point(cols, rows);
+        // XXX
+        self.points[0] = Point(1, 1);
+        self.points[1] = Point(1, 1);
+        self.points[2] = Point(1, 1);
+    }
+    fn land(&self, &mut rv Point[4]) {
+        // XXX
+        rv[0] = Point(2, 1);
+        rv[1] = Point(3, 1);
+        rv[2] = Point(4, 1);
+        rv[3] = Point(5, 1);
+    }
+}
+
+fn new_figure(&mut fig: GenericFigure) {
+    let bar = GenericFigure::bar(Point(1,1), [Point(1,1), Point(1,1), Point(1,1)]);
+    fig = bar;
 }
 
 // This basically exists in order to drop termion's RawTerminal restorer.
@@ -63,17 +134,21 @@ fn game() -> ExitCode {
     let stdin = stdin();
 
     // let mut field: Vec<Vec<bool>> = Vec::new();
-    // let mut field = vec![vec![false; COLS]; ROWS];
-    // let mut can = Can {
-    //     cols: COLS,
-    //     rows: ROWS,
-    //     matrix: field,
-    // };
+    let mut field = vec![vec![false; COLS]; ROWS];
+    let mut can = Can {
+        cols: COLS,
+        rows: ROWS,
+        matrix: field,
+    };
     let mut dp = DisplayOne {
         matrix: vec![vec![false; COLS]; ROWS],
         dp: &mut stdout,
     };
     dp.erase();
+
+    let curfig = GenericFigure();
+    new_figure(curfig, COLS, ROWS);
+    dp.update(can, curfig);
 
     dp.flush();
     for c in stdin.keys() {
